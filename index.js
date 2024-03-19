384d5f4f77027d8c059e1624ae6286fc7554d5d5
pk <papunku.pradhan273@gmail.com>
2024-03-19 11:18:33 +0530

    ABC commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const dob = '29/01/2000'
 function sayName() {
     console.log('my name is ' + name)
 }
-
-function isAbove18(){
-    return age >= 18
+function newFn(){
+    console.log('hey im a new fn')
 }
