338c187d8b0d962c0e74d46b00da1fbdab1f2e9a
pk <papunku.pradhan273@gmail.com>
2024-03-19 10:38:28 +0530

    remove 18 fn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const age = 24;
-const name1 = 'papun pradhan'
+const name = 'papun pradhan'
 const dob = '29/01/2000'
 
 
 function sayName() {
-    console.log('my name is ' + name1)
+    console.log('my name is ' + name)
 }
 
 function isAbove18(){
