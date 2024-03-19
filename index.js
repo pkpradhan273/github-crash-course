3f046171743489c4a4fbf25b6776ebb7fd4bf220
pk <papunku.pradhan273@gmail.com>
2024-03-19 08:37:07 +0530

    Added isAbove18 function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const age = 24;
-const name = 'papun pradhan'
+const name1 = 'papun pradhan'
 const dob = '29/01/2000'
 
 
 function sayName() {
-    console.log('my name is ' + name)
+    console.log('my name is ' + name1)
+}
+
+function isAbove18(){
+    return age >= 18
 }
